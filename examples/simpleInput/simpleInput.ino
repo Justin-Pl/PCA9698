359e58594db1d0bb54ff4bb1dc9d1acdbb130b94
Justin-Pl <109871921+Justin-Pl@users.noreply.github.com>
2022-07-26 09:04:28 +0200

    Update simpleInput.ino

--- examples/simpleInput/simpleInput.ino
+++ examples/simpleInput/simpleInput.ino
@@ -6,19 +6,22 @@ PCA9698 gpio;
 
 void setup()
 {
+    gpio.begin();   //Begin I2C
+    
+    //Set all pins to input
     uint8_t modes[5] = {0x00, 0x00, 0x00, 0x00, 0x00};
     gpio.setModePorts(modes, sizeof(modes));
 }
 
 void loop()
 {
-    gpio.updateInput();
-    uint8_t input[5];
-    gpio.digitalReadPorts(input, sizeof(input));
-    for (uint8_t index = 0; index < sizeof(input); index++)
+    gpio.updateInput(); //Reads input over I2C
+    uint8_t input[5];   //Array for input data
+    gpio.digitalReadPorts(input, sizeof(input));    //Reads all inputs 
+    for (uint8_t index = 0; index < sizeof(input); index++) 
     {
-        Serial.print(index, DEC);
-        Serial.println(input[index]);
+        Serial.print(index, DEC);       //Print port number
+        Serial.println(input[index]);   //Print port as HEX
     }
     delay(100);
 }
