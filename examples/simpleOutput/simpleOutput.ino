f710273dfaa45a44d3e987f651384cfbc2c4f779
Justin-Pl <109871921+Justin-Pl@users.noreply.github.com>
2022-07-26 09:06:35 +0200

    Update simpleOutput.ino

--- examples/simpleOutput/simpleOutput.ino
+++ examples/simpleOutput/simpleOutput.ino
@@ -2,20 +2,20 @@
 #include <PCA9698.h>
 
 /* Class Constructor */
-PCA9698 driver;
+PCA9698 gpio;
 
 void setup()
 {
     gpio.begin();   //Init I2C
     
     uint8_t modes[5] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
-    driver.setModePorts(modes, sizeof(modes));  //Set all pins as output
+    gpio.setModePorts(modes, sizeof(modes));  //Set all pins as output
 }
 
 void loop()
 {
     uint8_t toggle[5] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
-    driver.togglePorts(toggle, sizeof(toggle)); //Toggle all pins
-    driver.updateOutput();  //Send outputs to PCA9698
+    gpio.togglePorts(toggle, sizeof(toggle)); //Toggle all pins
+    gpio.updateOutput();  //Send outputs to PCA9698
     delay(1000);
 }
